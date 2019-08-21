5bc74358e0405764c101ea270e5551e68df4713d
Campbell Oleson <campbellsoleson@gmail.com>
2019-08-20 20:25:11 -0600

    resolving conflicts

--- frontend/src/components/weather/weather.jsx
+++ frontend/src/components/weather/weather.jsx
@@ -14,20 +14,17 @@ class Weather extends React.Component {
     this.weatherBalloon = this.weatherBalloon.bind(this);
   }
 
-<<<<<<< HEAD
   weatherBalloon(lat, lng) {
     var apiKey = "0dcf4810c5eab930389294cf28665b02";
-    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${Math.floor(
+    let url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${Math.floor(
       lat
     )}&lon=${Math.floor(lng)}&units=imperial&appid=${apiKey}`;
 
     request(url, (err, response, body) => {
       if (err) {
-        // console.log("error:", err);
+        console.log("error:", err);
       } else {
-        // console.log("body:", body);
         let weather = JSON.parse(body);
-        // let weatherr = JSON.parse(body)
         this.setState(state => {
           return {
             temp: weather.main.temp,
@@ -35,36 +32,11 @@ class Weather extends React.Component {
             speed: weather.wind.speed,
             dir: weather.wind.deg,
             humidity: weather.main.humidity
-            // weather: weather.main
           };
         });
       }
     });
   }
-=======
-    weatherBalloon(lat, lng) {
-        var apiKey = '0dcf4810c5eab930389294cf28665b02';
-        let url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${Math.floor(lat)}&lon=${Math.floor(lng)}&units=imperial&appid=${apiKey}`;
-
-        request(url,  (err, response, body) => {
-            if (err) {
-                console.log('error:', err);
-            } else {
-                let weather = JSON.parse(body)
-                this.setState((state) => {
-                    return {
-                        temp: weather.main.temp,
-                        conditions: weather.weather[0].description,
-                        speed: weather.wind.speed,
-                        dir: weather.wind.deg,
-                        humidity: weather.main.humidity,
-                    }
-                })
-            }
-        })
-        
-    }
->>>>>>> c4470f1977ec14bd935e1dd198441a38de78a1fe
 
   componentDidMount() {
     const { spot } = this.props;
@@ -73,13 +45,6 @@ class Weather extends React.Component {
 
   render() {
     const { conditions, temp, speed, dir, humidity } = this.state;
-
-<<<<<<< HEAD
-    const sky = this.state.conditions
-      ? this.state.conditions.includes("cloud")
-        ? "https://ripcamp-dev.s3-us-west-1.amazonaws.com/cloudy.jpg"
-        : "https://ripcamp-dev.s3-us-west-1.amazonaws.com/clear-sky.jpg"
-      : null;
 
     const icon = this.state.conditions ? (
       this.state.conditions.includes("cloud") ? (
@@ -91,7 +56,6 @@ class Weather extends React.Component {
 
     return (
       <div className="weather">
-        {/* <img src={sky} alt="" className="weather-pic"/> */}
         {icon}
         <h1 id="temp">{temp}&#176;F</h1>
         <p id="conditions">{conditions}</p>
@@ -104,27 +68,6 @@ class Weather extends React.Component {
       </div>
     );
   }
-=======
-        const icon = 
-            this.state.conditions ?
-                this.state.conditions.includes("cloud") ?
-                    <i class="fas fa-cloud fa-5x"></i>
-                :
-                    <i className="fas fa-sun fa-5x"></i>
-                : null;
-        
-        
-        return (
-            <div className="weather">
-                {icon}
-                <h1 id="temp">{temp}&#176;F</h1>
-                <p id="conditions">{conditions}</p>
-                <p id="wind-speed">Wind: {speed} mph at {dir} &#176;</p>
-                <p>Humidity: {humidity} g/m<sup>3</sup></p>
-            </div>
-        )
-    }
->>>>>>> c4470f1977ec14bd935e1dd198441a38de78a1fe
 }
 
 export default Weather;
